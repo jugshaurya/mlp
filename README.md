e6bb93d5c26e9042d87a33d560e8898d891c7fca
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2020-03-02 17:29:23 +0530

    :horse: added nextjs to client and server is setup with db, cors, routers, services.

---
+++ README.md
@@ -0,0 +1,23 @@
+## Learning NEXT.js
+
+🐳 Next.js is a react framework means it consist of all the functionality that we
+may require anytime during app development by wrting the javascript in form of
+React JSX, component, Declarative, state, props way.
+
+Next.js is used for serving react code to browser but by the means of Server side Rendering. Cool Intro right!🐳
+
+# Setup
+
+https://nextjs.org/learn/basics/getting-started/setup
+
+npm init -y
+npm install react react-dom next
+mkdir pages // next needs a page directory with it to work
+
+### Start Client
+
+npm run dev
+
+### Start Server
+
+npm start
--- server/index.js
+++ server/index.js
@@ -3,14 +3,15 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/mainrouter.js");
 //  App creation
 const app = express();
 
 // Basic Configurtions
 require("dotenv").config();
 const { CLIENT_URL, PORT, NODE_ENV } = process.env;
-// const whitelist = [CLIENT_URL, "http://localhost:8083/"];
+// const whitelist = [CLIENT_URL];
 // const corsOptions = {
 //   origin: function(origin, callback) {
 //     if (whitelist.indexOf(origin) !== -1) {
@@ -20,13 +21,24 @@ const { CLIENT_URL, PORT, NODE_ENV } = process.env;
 //     }
 //   }
 // };
+
+let mongodb_URL = process.env.MONGODB_URL;
 if (NODE_ENV === "development") {
+  mongodb_URL = process.env.MONGODB_LOCAL_URL;
   app.use(morgan("tiny"));
 }
+// DB connection
+mongoose
+  .connect(mongodb_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB Connected"));
 
 // Middleware Inplaced
 // app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors({ origin: CLIENT_URL }));
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,9 +48,7 @@ app.get("/", (req, res, next) => {
   res.json({ message: "Go to /api/v1" });
 });
 
-app.get("/api/v1", (req, res, next) => {
-  res.json({ time: JSON.stringify(Date()) });
-});
+app.get("/api/v1", mainRouter);
 
 // app listening on port PORT :)
 const port = PORT || 8083;
